84acc14bba433c1ff55ac4dd0fb57a249bc100ec
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-14 03:20:24 -0700

    Fixing type issues

--- source.js
+++ source.js
@@ -1,8 +1,8 @@
 import mergeDeepRight from "@unction/mergedeepright"
 import recordfrom from "@unction/recordfrom"
 
-export default function computedProp (computer: IterableType => any): Function {
-  return function computedPropComputer (keychain: KeyChain): Function {
+export default function computedProp (computer: IterableType => mixed): Function {
+  return function computedPropComputer (keychain: KeyChainType): Function {
     const receiver = recordfrom(keychain)
 
     return function computedPropComputerKeyChain (iterable: IterableType): IterableType {
