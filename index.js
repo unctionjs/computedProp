03f439cdf0fac7204aad6bd81ceb69780254812d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import mergeDeepRight from "@unction/mergedeepright"
-import recordfrom from "@unction/recordfrom"
-
-export default function computedProp (computer: FunctorType => mixed): Function {
-  return function computedPropComputer (keychain: KeyChainType): Function {
-    const receiver = recordfrom(keychain)
-
-    return function computedPropComputerKeyChain (functor: FunctorType): FunctorType {
-      return mergeDeepRight(functor)(receiver(computer(functor)))
-    }
-  }
+import mergeDeepRight from "@unction/mergedeepright";
+import recordfrom from "@unction/recordfrom";
+export default function computedProp(computer) {
+  return function computedPropComputer(keychain) {
+    const receiver = recordfrom(keychain);
+    return function computedPropComputerKeyChain(functor) {
+      return mergeDeepRight(functor)(receiver(computer(functor)));
+    };
+  };
 }
