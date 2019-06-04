4928824e007187700af373541dcf19160c2aaecb
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-06-03 23:14:50 -0700

    Switching away from deprecated functions

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import mergeDeepRight from "@unction/mergedeepright";
-import recordfrom from "@unction/recordfrom";
+import objectFrom from "@unction/objectfrom";
+
 export default function computedProp (computer) {
   return function computedPropComputer (keychain) {
-    const receiver = recordfrom(keychain);
+    const receiver = objectFrom(keychain);
 
 
     return function computedPropComputerKeyChain (functor) {
